9ff2d30b14fe28f92b5ba368b26f5a6964f30b86
Marcelo <marcelobonowott@yahoo.com.br>
2018-03-28 01:35:08 -0300

    Adicionado imagem na pasta correta

--- PC/Robo_Reconhecimento/Robo_Reconhecimento/main.cpp
+++ PC/Robo_Reconhecimento/Robo_Reconhecimento/main.cpp
@@ -1,14 +1,27 @@
 ///Programa para testar o reconhecimento de imagem no pc
 #include <stdio.h>
+#include <locale.h>
 
 int main()
 {
+	setlocale(LC_ALL, "pt-BR");
 	FILE* f = fopen("../linha.bmp", "rb");
+	if (f == NULL)
+	{
+		printf("não existe arquivo: linha.bmp");
+		getchar();
+		return -1;
+	}
 	unsigned char info[54];
 	fread(info, sizeof(unsigned char), 54, f);
 
-	int size = info[2] + (255 * info[3]) + (65535*info[4]) + (16777215 * info[5]);
-	printf("Tamanho: %d Bytes\n", size);
+	int totalSize = info[2] + (255 * info[3]) + (65535*info[4]) + (16777215 * info[5]);
+	int width = info[18];
+	int heigth = info[22];
+	int pixelSize = 3 * width * heigth;
+	printf("Tamanho: %d Bytes\n", totalSize);
+	printf("Altura: %d\n", heigth);
+	printf("Largura: %d\n", width);
 	for (int i = 0; i < 54; i++)
 	{
 		printf("Valor: %d\t No endereco: %x\n", info[i],i);
