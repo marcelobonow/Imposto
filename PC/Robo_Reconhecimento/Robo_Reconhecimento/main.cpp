9856b8b283e8058adc13b70962b251d6d0c82e7f
Marcelo <marcelobonowott@yahoo.com.br>
2018-03-28 01:59:47 -0300

    Alocado espaço para os pixeis

--- PC/Robo_Reconhecimento/Robo_Reconhecimento/main.cpp
+++ PC/Robo_Reconhecimento/Robo_Reconhecimento/main.cpp
@@ -1,6 +1,18 @@
-///Programa para testar o reconhecimento de imagem no pc
+/// Programa para testar o reconhecimento de imagem no pc
+/// O Windows guarda o programa em formato B G R na memória
 #include <stdio.h>
+#include <stdlib.h>
 #include <locale.h>
+#include <stdint.h>
+
+#include "types.h"
+
+struct RGB
+{
+	uint8 R;
+	uint8 G;
+	uint8 B;
+};
 
 int main()
 {
@@ -10,7 +22,7 @@ int main()
 	{
 		printf("não existe arquivo: linha.bmp");
 		getchar();
-		return -1;
+		exit(EXIT_FAILURE);
 	}
 	unsigned char info[54];
 	fread(info, sizeof(unsigned char), 54, f);
@@ -18,16 +30,23 @@ int main()
 	int totalSize = info[2] + (255 * info[3]) + (65535*info[4]) + (16777215 * info[5]);
 	int width = info[18];
 	int heigth = info[22];
-	int pixelSize = 3 * width * heigth;
+	int bitsPerPixel = info[34];
+	int imageSize = (bitsPerPixel/8) *width * heigth;
+	printf("----- HEADER ------\n");
 	printf("Tamanho: %d Bytes\n", totalSize);
 	printf("Altura: %d\n", heigth);
 	printf("Largura: %d\n", width);
-	for (int i = 0; i < 54; i++)
+	
+	///Pega a imagem em si
+	unsigned char* data = (unsigned char*) malloc(imageSize);
+	if (data == NULL)
 	{
-		printf("Valor: %d\t No endereco: %x\n", info[i],i);
-
+		perror("Não foi possivel alocar o tamanho da imagem");
+		getchar();
+		exit(EXIT_FAILURE);
 	}
 
+	free(data);
 	getchar();
 
 }
---
+++ PC/Robo_Reconhecimento/Robo_Reconhecimento/types.h
@@ -0,0 +1,9 @@
+#pragma once
+
+#define uint8 uint8_t
+#define uint16 uint16_t
+#define uint32 uint32_t
+
+#define int8 int8_t
+#define int16 int16_t
+#define int32 int32_t
