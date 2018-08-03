304b7f1315eacff65e5d18fb6ab2dab4da9ebd48
LAB 14 - 2 <marcelobonowott@hotmail.com>
2018-08-02 21:17:05 -0300

    Commit

---
+++ main.h
@@ -0,0 +1,11 @@
+#include <18F4550.h>
+#device ADC=10
+
+#FUSES NOWDT                 	//No Watch Dog Timer
+#FUSES WDT128                	//Watch Dog Timer uses 1:128 Postscale
+#FUSES NOBROWNOUT            	//No brownout reset
+#FUSES NOLVP                 	//No low voltage prgming, B3(PIC16) or B5(PIC18) used for I/O
+#FUSES NOXINST               	//Extended set extension and Indexed Addressing mode disabled (Legacy mode)
+
+#use delay(clock=48MHz,crystal=20MHz)
+
