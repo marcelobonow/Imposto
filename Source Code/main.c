6ddb6f7150a64fb7c331968a61f3bf485ba83388
Marcelo Bonow <marcelobonowott@yahoo.com.br>
2018-08-29 08:07:45 -0300

    Continuação pista

--- Source Code/main.c
+++ Source Code/main.c
@@ -27,13 +27,18 @@ void Setup();
 void AndarReto();
 ///Se tiver retorna 1
 int8 IsCenterAlign();
+int8 IsCenterAndNotMargins();
+int8 IsCenterInside();
+int8 IsMarginsInside();
+int8 IsMarginsOutside();
+int8 IsAllOutside();
 ///Curva a direita
 int8 IsRight();
 int8 IsLeft();
 int8 IsAllAlign();
-int8 IsCenterAndNotMargins();
 ///uma linha preta sobre todos
 void Align();
+void InvertedAlign();
 
 #int_timer0
 void timerTick()
@@ -127,10 +132,11 @@ void main()
         }
         else if (decisionCounter == 1 && running)
         {
+            ///Se alinha ate achar a curva a direita
             Align();
-            ///TODO: Dobrar a direita
             if (IsRight() && !decisionInCoolDown)
             {
+                ///Começa a fazer a curva a direita girando um pouco para a direita e dando uma ré
                 SetLeft(900);
                 SetRight(950);
                 RightBackwardLeftForward();
@@ -148,30 +154,193 @@ void main()
             }
             else
             {
-                ///Continua o que tava fazendo
+                ///Continua Girando do jeito que estava ate ficar com os 2 sensores no centro
             }
         }
         else if (decisionCounter == 3)
         {
+            ///Anda alinhado até achar a linha preta
             Align();
             if (IsAllAlign() && !decisionInCoolDown)
             {
-                ///TODO: Dobrar a direita
+                SetLeft(900);
+                SetRight(950);
+                RightBackwardLeftForward();
+                delay_ms(500);
+                decisionCounter++;
+                decisionInCoolDown = 25;
             }
+        }
+        else if (decisionCounter == 4)
+        {
+            ///Primeira linha de parada
+            Align();
+            if (IsAllAlign() && !decisionInCoolDown)
+            {
+                decisionCounter++;
+                decisionInCoolDown = 10;
+            }
+        }
+        else if (decisionCounter == 5)
+        {
+            ///Logo após passar a primeira linha de parada, anda alinhado até o decisioninCoolDown e para
+            Align();
+            if (!decisionInCoolDown)
+            {
+                SetBothPwm(0);
+                delay_ms(2000);
+                SetBothPwm(920);
+                decisionCounter++;
+            }
+        }
+        else if (decisionCounter == 6)
+        {
+            ///Logo após sair da parada, anda alinhado até achar a proxima linha completa
+            Align();
+            if (IsAllAlign() && !decisionInCoolDown)
+            {
+                decisionCounter++;
+                decisionInCoolDown = 5;
+            }
+        }
+        else if (decisionCounter == 7)
+        {
+            ///Passou pela linha completa do desafio de parar anda alinhado até a linha da esquerda do desafio de andar invertido
+            Align();
+            if (IsLeft() && !decisionInCoolDown)
+            {
+                decisionCounter++;
+                decisionInCoolDown = 0;
+            }
+        }
+        else if (decisionCounter == 8)
+        {
+            ///Anda alinhado até chegar no desafio da linha invertida, a linha invertida começa quando o centro esta fora e as extremidades dentro
+
+            /*!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+            !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+                FREDERICO, TALVEZ AQUI SEJA BOM
+                ALINHAR COM UM VALOR MENOR DE PWM
+                OU ALINHAR DE FORMA MAIS SENSIVEL
+            !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+            */
+            Align();
+            if (IsMarginsInside() && !decisionInCoolDown)
+            {
+                ///Iniciou o desafio de linha invertido
+                decisionCounter++;
+                decisionInCoolDown = 20;
+            }
+        }
+        else if (decisionCounter == 9)
+        {
+            ///Desafio da linha invertida
+            InvertedAlign();
+            ///Anda ate não ter linha nas extremidades
+            if (IsMarginsOutside() && !decisionInCoolDown)
+            {
+                decisionCounter++;
+                decisionInCoolDown = 5;
+            }
+        }
+        else if (decisionCounter == 10)
+        {
+            ///Fim do desafio de linha invertida
+            Align();
+            if (IsLeft() && !decisionInCoolDown)
+            {
+                decisionCounter++;
+                decisionInCoolDown = 8;
+            }
+        }
+        ///11 e 12 curva esquerda
+        else if (decisionCounter == 11)
+        {
+            ///Curva 90 graus a esquerda
+            Align();
+            if (IsLeft() && !decisionInCoolDown)
+            {
+                ///TODO: Curva esquerda
+                SetLeft(950);
+                SetRight(900);
+                LeftBackwardRightForward();
+                delay_ms(500);
+                decisionCounter++;
+                decisionInCoolDown = 25;
+            }
+        }
+        else if (decisionCounter == 12)
+        {
+
+            Align();
+            if (IsLeft() && !decisionInCoolDown)
+            {
+                decisionCounter++;
+                decisionInCoolDown = 5;
+            }
+        }
+        else if (decisionCounter == 13)
+        {
+            ///Desafio de andar cego
+            ///!!!!!!!!!!   AQUI ELE TERIA QUE ESTAR MUITO BEM ALINHADO, SERIA BOM TER A FUNÇÃO DE ALINHAR E COLOCAR PARA ALINHAR BEM AQUI
+            Align();
+            if (IsAllOutside() && !decisionInCoolDown)
+            {
+                decisionCounter++;
+                decisionInCoolDown = 25;
+            }
+        }
+        else if(decisionCounter == 14)
+        {
+            ///ESTA NO DESAFIO DE ANDAR SEM LINHA
+            SetBothPwm(870);
+            if(IsCenterInside() && !decisionInCoolDown)
+            {
+                Align();
+                decisionCounter++;
+                decisionInCoolDown = 5;
+            }
+        }
+        else if(decisionCounter == 15)
+        {
+            ///Passou pelo desafio de andar sem linha
+            Align();
+            if(IsLeft() && !decisionInCoolDown)
+            {
+                decisionCounter++;
+                decisionInCoolDown = 8;
+            }
+        }
+        else if(decisionCounter == 16)
+        {
+            Align();
+            if(IsRight() && !decisionInCoolDown)
+            {
+                decisionCounter++;
+                decisionInCoolDown = 8;
+            }
+        }
+        else if(decisionCounter == 17)
+        {
+            ///TODO: desvia do desafio de remover o bloco
+           SetBothPwm(0);
+           lcd_pos_xy(1,1);
+           printf(lcd_escreve, "Fim do programado");
+        }
+
+        else
+        {
+            ///Se ja passou por todas as decisões
+            SetBothPwm(0);
         }
 
         if (running == 0)
         {
             SetBothPwm(0);
+            SetBothPwm(0);
         }
     } while (1);
 }
-
-void AndarReto()
-{
-    SetBothPwm(1023);
-}
-
 int8 IsCenterAlign()
 {
     float ad2Ratio = (float)ad2 / (float)(ad1 + ad2);
@@ -199,6 +368,50 @@ int8 IsRight()
 int8 IsCenterAndNotMargins()
 {
     if (ad1 < 300 && ad2 >= 300 && ad3 >= 300 && ad4 < 300)
+    {
+        return 1;
+    }
+    else
+    {
+        return 0;
+    }
+}
+int8 IsCenterInside()
+{
+    if(ad2 >= 300 && ad3 >= 300)
+    {
+        return 1;
+    }
+    else
+    {
+        return 0;
+    }
+}
+int8 IsMarginsInside()
+{
+    if (ad1 >= 300 && ad4 >= 400)
+    {
+        return 1;
+    }
+    else
+    {
+        return 0;
+    }
+}
+int8 IsMarginsOutside()
+{
+    if (ad1 < 300 && ad4 < 400)
+    {
+        return 1;
+    }
+    else
+    {
+        return 0;
+    }
+}
+int8 IsAllOutside()
+{
+    if (ad1 < 300 && ad2 < 300 && ad3 < 300 && ad4 < 300)
     {
         return 1;
     }
@@ -259,11 +472,11 @@ void Align()
         else
         {
 
-            ///777 é o minimo que o motor vai ter (ele precisa de uns 800 pra não ficar travado) e vai somar no maximo 246
-            ///Por isso que o maximo dele é 1023 (777 + 246), é possivel fazer com que o valor de maior que 1023 e limitar
+            ///850 é o minimo que o motor vai ter (ele precisa de uns 800 pra não ficar travado) e vai somar no maximo 173
+            ///Por isso que o maximo dele é 1023 (850 + 173), é possivel fazer com que o valor de maior que 1023 e limitar
             ///Com um if, da para testar isso
-            long pwm1 = ad2Ratio * 246 + 777;
-            long pwm2 = ad3Ratio * 246 + 777;
+            long pwm1 = ad2Ratio * 173 + 850;
+            long pwm2 = ad3Ratio * 173 + 850;
             SetLeft(pwm1);
             SetRight(pwm2);
             frente();
@@ -281,8 +494,56 @@ void Align()
     }
 }
 
+///BASICAMENTE faz a mesma coisa que o algin, mas troca os sensores (coloca o da esquerda na direitea)
 void InvertedAlign()
 {
+    float ad2Ratio = (float)ad2 / (float)(ad2 + ad3);
+    float ad3Ratio = (float)ad3 / (float)(ad2 + ad3);
+
+    ///alto significa escuro
+    if (ad2 + ad3 <= 400)
+    {
+        //Como quanto mais escuro mais alto, isso significa que o que esta com menos tem que subir,
+        //e vai subir proporcionalmente a quanto a menos ele tem, esse valor é exatamente o oposto (que esta no outro ad)
+
+        lcd_pos_xy(1, 1);
+
+        //O ad2 é o da direita, se este esta maior, significa que o robo esta a direita
+        if (ad3Ratio > alignRatio)
+        {
+            printf(lcd_escreve, "Esta a direita");
+            SetBothPwm(950);
+            LeftBackwardRightForward();
+        }
+        else if (ad2Ratio > alignRatio)
+        {
+            printf(lcd_escreve, "Esta a esquerda");
+            SetBothPwm(950);
+            RightBackwardLeftForward();
+        }
+        else
+        {
+
+            ///850 é o minimo que o motor vai ter (ele precisa de uns 800 pra não ficar travado) e vai somar no maximo 173
+            ///Por isso que o maximo dele é 1023 (850 + 173), é possivel fazer com que o valor de maior que 1023 e limitar
+            ///Com um if, da para testar isso
+            long pwm1 = ad2Ratio * 173 + 850;
+            long pwm2 = ad3Ratio * 173 + 850;
+            SetLeft(pwm2);
+            SetRight(pwm1);
+            frente();
+            // SetLeft((long)(ad2Ratio * 100) + 850);
+            // SetRight((long)(ad3Ratio * 100) + 850);
+        }
+    }
+    else
+    {
+        ///Esta fora da pista, deve fazer algo
+        //SetBothPwm(0);
+        lcd_pos_xy(1, 2);
+
+        printf(lcd_escreve, "Se perdeu");
+    }
 }
 
 void Setup()
